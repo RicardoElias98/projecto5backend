35145b1854be40209c27c97521e597856a65a1c8
ricardoelias98 <uc2016225298@student.uc.pt>
2024-04-16 17:14:10 +0100

    inicio do websocket das tasks

---
+++ src/main/java/websocket/WebSocketTasks.java
@@ -0,0 +1,81 @@
+package websocket;
+
+import bean.UserBean;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.Mensage;
+import dto.Notification;
+import entities.UserEntity;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Singleton;
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import utilities.LocalDateTimeAdapter;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+@Singleton
+@ServerEndpoint("/tasks/{token}")
+public class WebSocketTasks {
+
+    private UserBean userbean;
+
+    HashMap<String, Session> sessions = new HashMap<String, Session>();
+
+    public void send(String token, String msg) {
+        Session session = sessions.get(token);
+        if (session != null) {
+            System.out.println("sending.......... " + msg);
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                System.out.println("Something went wrong!");
+            }
+        }
+    }
+
+    @OnOpen
+    public void toDoOnOpen(Session session, @PathParam("token") String token) {
+        System.out.println("A new WebSocketNotification session is opened for client with token: " + token);
+        sessions.put(token, session);
+    }
+
+    @OnClose
+    public void toDoOnClose(Session session, CloseReason reason) {
+        System.out.println("WebsocketNotification session is closed with CloseCode: " +
+                reason.getCloseCode() + ": " + reason.getReasonPhrase());
+        for (String key : sessions.keySet()) {
+            if (sessions.get(key) == session) sessions.remove(key);
+        }
+    }
+
+    @OnMessage
+    public void toDoOnMessage(String notification) throws NamingException {
+        InitialContext ctx = new InitialContext();
+        userbean = (UserBean) ctx.lookup("java:module/UserBean");
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+        Notification notiAgain = gson.fromJson(notification, Notification.class);
+        UserEntity user = notiAgain.getUser();
+        String username = user.getUsername();
+        String tokenUser = userbean.getUserByUsername(username).getToken();
+        Session receiverSession = sessions.get(tokenUser);
+        if (receiverSession!= null) {
+            try {
+                receiverSession.getBasicRemote().sendObject(notification);
+            } catch (IOException e) {
+                System.out.println("Something went wrong!");
+            } catch (EncodeException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
